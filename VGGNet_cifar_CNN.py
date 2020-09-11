de22001a303f4a44cb830fcd6df99782d0fc2ec9
MinWookChoi <cmw1017@naver.com>
2020-09-11 17:02:00 +0900

    update 0911

--- VGGNet_cifar_CNN.py
+++ VGGNet_cifar_CNN.py
@@ -11,7 +11,7 @@ import torchvision.models.vgg as vgg
 # CUDA 사용시 적용
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 torch.manual_seed(777)
-if device =='cuda':
+if device == 'cuda':
     torch.cuda.manual_seed_all(777)
 
 # cifar 데이터 로드
@@ -30,12 +30,12 @@ testset = torchvision.datasets.CIFAR10(root='./data/cifar10', train=False,
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                          shuffle=False, num_workers=0)
 
-classes = ('plane', 'car', 'bird', 'cat','deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
 # 이미지 보여주기 함수
 def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
+    img = img / 2 + 0.5  # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
@@ -49,11 +49,12 @@ images, labels = dataiter.next()
 
 # VGG-11~19까지 선택
 cfg = {
-    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'], #8 + 3 =11 == vgg11
-    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'], # 10 + 3 = vgg 13
-    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'], #13 + 3 = vgg 16
-    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'], # 16 +3 =vgg 19
-    'custom' : [64,64,64,'M',128,128,128,'M',256,256,256,'M']
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],  # 8 + 3 =11 == vgg11
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],  # 10 + 3 = vgg 13
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],  # 13 + 3 = vgg 16
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+    # 16 +3 =vgg 19
+    'custom': [64, 64, 64, 'M', 128, 128, 128, 'M', 256, 256, 256, 'M']
 }
 
 
@@ -117,19 +118,45 @@ class VGG(nn.Module):
 
 
 # VGG 설정, cfg의 타입에 맞게 설정하여야함
-vggN = VGG(vgg.make_layers(cfg['A']), 10, True).to(device) 
-load_model = Path("./model/VGG11_cifar_CNN")
-if load_model.is_file():
-    vggN.load_state_dict(torch.load('./model/VGG11_cifar_CNN'))
-    print("load saved model file")
+vggN = VGG(vgg.make_layers(cfg['A']), 10, True).to(device)
+
+# load_model = Path("./model/VGG11_cifar_CNN")
+# if load_model.is_file():
+#     vggN.load_state_dict(torch.load('./model/VGG11_cifar_CNN'))
+#     print("load saved model file")
 
 criterion = nn.CrossEntropyLoss().to(device)
 optimizer = torch.optim.SGD(vggN.parameters(), lr=0.005, momentum=0.9)
 
 lr_sche = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.9)
 
+
+def acc_check(net, test_set, epoch, save=1):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_set:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = net(images)
+
+            _, predicted = torch.max(outputs.data, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    acc = (100 * correct / total)
+    print('Accuracy of the network on the 10000 test images: %d %%' % acc)
+    if save:
+        torch.save(net.state_dict(), "./model/VGGNet11_epoch_{}_acc_{}.pth".format(epoch, int(acc)))
+    return acc
+
+
 # Train
-epochs = 2
+print(len(trainloader))
+epochs = 8
+
 for epoch in range(epochs):  # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
@@ -150,43 +177,11 @@ for epoch in range(epochs):  # loop over the dataset multiple times
         # print statistics
         running_loss += loss.item()
         if i % 5 == 4:  # print every 5 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 5))
+            print('[%d, %5d, %d%%] loss: %.3f' %
+                  (epoch + 1, i + 1, 100 * (i + 1) / len(trainloader), running_loss / 5))
             running_loss = 0.0
     lr_sche.step()
+    # Check Accuracy
+    acc = acc_check(vggN, testloader, epoch, save=1)
+
 print('Finished Training')
-
-# Test 이미지 보여주기
-dataiter = iter(testloader)
-images, labels = dataiter.next()
-imshow(torchvision.utils.make_grid(images[0]))
-print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(1)))
-
-# Test Predict 결과 보여주기
-outputs = vggN(images.to(device))
-_, predicted = torch.max(outputs, 1)
-
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
-
-
-# Test Starting
-correct = 0
-total = 0
-
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        images = images.to(device)
-        labels = labels.to(device)
-        outputs = vggN(images)
-
-        _, predicted = torch.max(outputs.data, 1)
-
-        total += labels.size(0)
-
-        correct += (predicted == labels).sum().item()
-
-print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
-
-torch.save(vggN.state_dict(), "./model/VGG11_cifar_CNN")
-print("model saved complete")
